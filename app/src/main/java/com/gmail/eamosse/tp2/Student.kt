6564c9ea9d7eee9a40d5b79fd5ff6ea37e87c711
IlanOu <71207643+IlanOu@users.noreply.github.com>
2024-11-12 15:46:26 +0100

    fill methods

--- app/src/main/java/com/gmail/eamosse/tp2/Student.kt
+++ app/src/main/java/com/gmail/eamosse/tp2/Student.kt
@@ -1,4 +1,4 @@
 package com.gmail.eamosse.tp2
 
 //TODO : Complete the class
-data class Student(val name : String, val code : String, val address : String, val age : Int)
+data class Student(val name : String, val code : String, val address : String, val age : Int, val sexe: String)
